700ab546213150ffaded4b18e521d70df4f01f0f
Truongle68 <truonglephuc11@gmail.com>
2025-07-06 21:39:02 +0700

    Build navigation

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -8,10 +8,29 @@ import LoginScreen from "../screens/Auth/LoginScreen";
 import RegisterScreen from "../screens/Auth/RegisterScreen";
 import HomeScreen from "../screens/Main/HomeScreen";
 
-import { AuthStackParamList, MainStackParamList } from "../types/navigation";
+import {
+  AuthStackParamList,
+  HomeStackParamList,
+  NotificationStackParamList,
+  ProfileStackParamList,
+  SearchStackParamList,
+  TabParamList,
+} from "../types/navigation";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ProductDetailScreen from "../screens/Main/ProductDetailScreen";
+import ProfileScreen from "../screens/Main/ProfileScreen";
+import SettingsScreen from "../screens/Main/SettingsScreen";
+import NotificationScreen from "../screens/Main/NotificationScreen";
+import SearchScreen from "../screens/Main/SearchScreen";
+import ProductListScreen from "../screens/Main/ProductListScreen";
 
 const AuthStack = createNativeStackNavigator<AuthStackParamList>();
-const MainStack = createNativeStackNavigator<MainStackParamList>();
+const HomeStack = createNativeStackNavigator<HomeStackParamList>();
+const ProfileStack = createNativeStackNavigator<ProfileStackParamList>();
+const SearchStack = createNativeStackNavigator<SearchStackParamList>();
+const NotificationStack =
+  createNativeStackNavigator<NotificationStackParamList>();
+const Tab = createBottomTabNavigator<TabParamList>();
 
 const AuthNavigator = () => (
   <AuthStack.Navigator screenOptions={{ headerShown: false }}>
@@ -20,14 +39,94 @@ const AuthNavigator = () => (
   </AuthStack.Navigator>
 );
 
-const MainNavigator = () => (
-  <MainStack.Navigator>
-    <MainStack.Screen
+const HomeNavigator = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen
       name="Home"
       component={HomeScreen}
       options={{ title: "Trang chủ" }}
     />
-  </MainStack.Navigator>
+    <HomeStack.Screen
+      name="Detail"
+      component={ProductDetailScreen}
+      options={{ title: "Chi tiết sản phẩm" }}
+    />
+  </HomeStack.Navigator>
+);
+
+const ProfileNavigator = () => (
+  <ProfileStack.Navigator>
+    <ProfileStack.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{ title: "Thông tin cá nhân" }}
+    />
+    <ProfileStack.Screen
+      name="Settings"
+      component={SettingsScreen}
+      options={{ title: "Cài đặt" }}
+    />
+  </ProfileStack.Navigator>
+);
+
+const NotificationNavigator = () => (
+  <NotificationStack.Navigator>
+    <NotificationStack.Screen
+      name="Notification"
+      component={NotificationScreen}
+      options={{ title: "Thông báo" }}
+    />
+    <NotificationStack.Screen
+      name="Detail"
+      component={ProductDetailScreen}
+      options={{ title: "Chi tiết sản phẩm" }}
+    />
+  </NotificationStack.Navigator>
+);
+
+const SearchNavigator = () => (
+  <SearchStack.Navigator>
+    <SearchStack.Screen
+      name="Search"
+      component={SearchScreen}
+      options={{ title: "Tìm kiếm" }}
+    />
+    <SearchStack.Screen
+      name="ProductList"
+      component={ProductListScreen}
+      options={{ title: "Kết quả tìm kiếm" }}
+    />
+    <SearchStack.Screen
+      name="Detail"
+      component={ProductListScreen}
+      options={{ title: "Chi tiết sản phẩm" }}
+    />
+  </SearchStack.Navigator>
+);
+
+const BottomTab = () => (
+  <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Screen
+      name="Home"
+      component={HomeNavigator}
+      options={{ title: "Trang chủ" }}
+    />
+    <Tab.Screen
+      name="Search"
+      component={SearchNavigator}
+      options={{ title: "Tìm kiếm" }}
+    />
+    <Tab.Screen
+      name="Notification"
+      component={NotificationNavigator}
+      options={{ title: "Thông báo" }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={ProfileNavigator}
+      options={{ title: "Tài khoản" }}
+    />
+  </Tab.Navigator>
 );
 
 export const AppNavigator = () => {
@@ -43,7 +142,8 @@ export const AppNavigator = () => {
 
   return (
     <NavigationContainer>
-      {accessToken ? <MainNavigator /> : <AuthNavigator />}
+      {/* {accessToken ? <BottomTab /> : <AuthNavigator />} */}
+      {<BottomTab />}
     </NavigationContainer>
   );
 };
--- mobile/src/types/navigation.ts
+++ mobile/src/types/navigation.ts
@@ -6,3 +6,32 @@ export type AuthStackParamList = {
 export type MainStackParamList = {
   Home: undefined;
 };
+
+export type HomeStackParamList = {
+  Home: undefined;
+  Detail: undefined
+};
+
+export type ProfileStackParamList = {
+  Profile: undefined;
+  Settings: undefined
+};
+
+export type NotificationStackParamList = {
+  Notification: undefined;
+  Detail: undefined;
+};
+
+export type SearchStackParamList = {
+  Search: undefined;
+  ProductList: undefined;
+  Detail: undefined;
+};
+
+export type TabParamList = {
+  Home: undefined;
+  Profile: undefined;
+  Notification: undefined;
+  Search: undefined;
+  Cart: undefined;
+};
