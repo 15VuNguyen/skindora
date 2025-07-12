317d8d989f3acec5236ada356fc7278f54936ad2
Phan Văn Trường <phantryong@gmail.com>
2025-07-12 21:38:44 +0700

    Add get all product type for user

--- backend/src/routes/products.routes.ts
+++ backend/src/routes/products.routes.ts
@@ -4,7 +4,10 @@ import { isAdminValidator } from '~/middlewares/admin.middlewares'
 import { accessTokenValidator } from '~/middlewares/users.middlewares'
 import { wrapAsync } from '~/utils/handler'
 import { getProductDetailController } from '~/controllers/products.controllers'
+import { getAllFilterHskProductTypesController } from '~/controllers/filterHskProductType.controllers'
+
 const productRouter = Router()
 productRouter.get('/get-all', wrapAsync(userGetAllProductController))
+productRouter.get('/get-all-filter-hsk-product-types', wrapAsync(getAllFilterHskProductTypesController))
 productRouter.get('/:_id', wrapAsync(getProductDetailController))
 export default productRouter
